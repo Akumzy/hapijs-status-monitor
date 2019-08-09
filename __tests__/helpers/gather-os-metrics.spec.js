ffdcddc113f4cc750e9a1c57cf5663df42fea897
Akuma Isaac Akuma <akumaisaacakuma@gmail.com>
2019-08-09 07:12:12 +0100

    fixed husky deprecation issue

--- __tests__/helpers/gather-os-metrics.spec.js
+++ __tests__/helpers/gather-os-metrics.spec.js
@@ -1,12 +1,12 @@
-const gatherOsMetrics = require('../../src/helpers/gather-os-metrics');
+const pidusage = require("pidusage");
+const gatherOsMetrics = require("../../src/helpers/gather-os-metrics");
 
-jest.mock('pidusage');
-const pidusage = require('pidusage');
+jest.mock("pidusage");
 
-describe('helpers', () => {
-  describe('gather-os-metrics', () => {
-    describe('when invoked', () => {
-      it('then pidusage should be called', () => {
+describe("helpers", () => {
+  describe("gather-os-metrics", () => {
+    describe("when invoked", () => {
+      it("then pidusage should be called", () => {
         gatherOsMetrics({}, { os: [], responses: [] });
         expect(pidusage).toHaveBeenCalled();
       });
--- src/middleware-wrapper.js
+++ src/middleware-wrapper.js
@@ -1,36 +1,42 @@
-const fs = require('fs');
-const path = require('path');
-const validate = require('./helpers/validate');
-const onHeadersListener = require('./helpers/on-headers-listener');
-const socketIoInit = require('./helpers/socket-io-init');
+const fs = require("fs");
+const path = require("path");
+const validate = require("./helpers/validate");
+const onHeadersListener = require("./helpers/on-headers-listener");
+const socketIoInit = require("./helpers/socket-io-init");
 
 // hapi.js plugin register function
 const middlewareWrapper = (server, options) => {
   const opts = validate(options);
 
   // Setup Socket.IO
-  server.events.on('start', () => {
+  server.events.on("start", () => {
     socketIoInit(server.listener, opts.spans);
   });
 
   server.route({
-    method: 'GET',
+    method: "GET",
     path: opts.path,
     handler: () => {
-      const renderedHtml =
-        fs.readFileSync(path.join(__dirname, '/public/index.html'))
-          .toString()
-          .replace(/{{title}}/g, opts.title)
-          .replace(/{{script}}/g, fs.readFileSync(path.join(__dirname, '/public/javascripts/app.js')))
-          .replace(/{{style}}/g, fs.readFileSync(path.join(__dirname, '/public/stylesheets/style.css')));
+      const renderedHtml = fs
+        .readFileSync(path.join(__dirname, "/public/index.html"))
+        .toString()
+        .replace(/{{title}}/g, opts.title)
+        .replace(
+          /{{script}}/g,
+          fs.readFileSync(path.join(__dirname, "/public/javascripts/app.js"))
+        )
+        .replace(
+          /{{style}}/g,
+          fs.readFileSync(path.join(__dirname, "/public/stylesheets/style.css"))
+        );
 
       return renderedHtml;
     },
-    config: opts.routeConfig,
+    config: opts.routeConfig
   });
 
   // Hook into the middle of processing
-  server.ext('onPreResponse', (request, h) => {
+  server.ext("onPreResponse", (request, h) => {
     if (request.response.isBoom || request.path === opts.path) {
       return h.continue;
     }
@@ -38,7 +44,7 @@ const middlewareWrapper = (server, options) => {
     const startTime = process.hrtime();
     const resp = request.response;
 
-    resp.events.once('finish', () => {
+    resp.events.once("finish", () => {
       onHeadersListener(resp.statusCode, startTime, opts.spans);
     });
 
